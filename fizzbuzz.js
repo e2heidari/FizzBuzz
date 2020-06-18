f57d3a7d425316afac44e462306f6e5039b28082
Ehsan <Ehsan.heydari@gmail.com>
2020-06-17 19:23:30 -0700

    show fizz for % 3=0 show buzz for % 7=0 ,..

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    let text = ""
-    let counter;
+    let text = "";
     for (let counter = 1; counter <= 100; counter++) {
-        text += counter + " ";
-        if (text % 3 == 0) {
-            text = "Fizz"
+        text = counter + " ";
+        if (text % 3 == 0 && text % 7 == 0) {
+            text += "FizzBuzz"
         }
         else if (text % 7 == 0) {
-            text = "Buzz"
+            text += "Buzz"
         }
-        document.getElementById("numbers").innerText = text;
+        else if (text % 3 == 0) {
+            text += "Fizz"
+        }
+        else {
+            text += counter;
+        }
+        document.getElementById("numbers").innerText += text + "\r\n";
     }
 })
